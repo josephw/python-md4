42595d160f3168b8eebfc53907c28575165dd7ce
Joseph Walton <joe@kafsemo.org>
2013-05-12 22:55:49 +1000

    Add a test runner for the test vectors.

---
+++ md4_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import unittest
+import md4
+import binascii
+
+class TestProvidedVectors(unittest.TestCase):
+    def test_equals_expected(self):
+      for (msg, digest) in md4.md4_test:
+        m = md4.MD4()
+        m.update(msg.encode('us-ascii'))
+        d = m.digest()
+
+        self.assertEquals(binascii.hexlify(d), '%032x' % digest)
+
+if __name__ == '__main__':
+    unittest.main()
